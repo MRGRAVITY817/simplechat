f390b6b40cdfb935f10e6b1606d45da84e6a6c0c
Hoon Wee <mrgravity817@gmail.com>
2024-02-22 14:57:28 +0900

    feat: track presence

--- lib/simplechat_web/channels/room_channel.ex
+++ lib/simplechat_web/channels/room_channel.ex
@@ -1,5 +1,6 @@
 defmodule SimplechatWeb.RoomChannel do
   use SimplechatWeb, :channel
+  alias SimplechatWeb.Presence
 
   @impl true
   def join("room:lobby", payload, socket) do
@@ -22,11 +23,25 @@ defmodule SimplechatWeb.RoomChannel do
   # broadcast to everyone in the current topic (room:lobby).
   @impl true
   def handle_in("shout", payload, socket) do
-    Simplechat.Message.changeset(%Simplechat.Message{}, payload)
-    |> Simplechat.Repo.insert()
+    {:ok, msg} =
+      Simplechat.Message.changeset(%Simplechat.Message{}, payload)
+      |> Simplechat.Repo.insert()
 
-    broadcast(socket, "shout", payload)
+    # Assign name to socket assigns and track presence
+    socket
+    |> assign(:person_name, msg.name)
+    |> track_presence()
+    |> broadcast("shout", Map.put_new(payload, :id, msg.id))
+
     {:noreply, socket}
+  end
+
+  defp track_presence(%{assigns: %{person_name: person_name}} = socket) do
+    Presence.track(socket, person_name, %{
+      online_at: inspect(System.system_time(:second))
+    })
+
+    socket
   end
 
   # Send existing messages to the client when they join
@@ -41,6 +56,9 @@ defmodule SimplechatWeb.RoomChannel do
         inserted_at: message.inserted_at
       })
     end)
+
+    # Send currently active users
+    push(socket, "presence_state", Presence.list("room:lobby"))
 
     {:noreply, socket}
   end
