52f51b5d00b9061fd515be6f9eefc11f7e8a2794
Hoon Wee <mrgravity817@gmail.com>
2024-02-22 14:37:35 +0900

    test: :after_join

--- test/simplechat_web/channels/room_channel_test.exs
+++ test/simplechat_web/channels/room_channel_test.exs
@@ -24,4 +24,19 @@ defmodule SimplechatWeb.RoomChannelTest do
     broadcast_from!(socket, "broadcast", %{"some" => "data"})
     assert_push "broadcast", %{"some" => "data"}
   end
+
+  test ":after_join sends existing messages to the client", %{socket: socket} do
+    # Arrange
+    payload = %{name: "Alice", message: "Hello"}
+    Simplechat.Message.changeset(%Simplechat.Message{}, payload) |> Simplechat.Repo.insert()
+
+    # Act
+    {:ok, _, socket2} =
+      SimplechatWeb.UserSocket
+      |> socket("user_id", %{some: :assign})
+      |> subscribe_and_join(SimplechatWeb.RoomChannel, "room:lobby")
+
+    # Assert
+    assert socket2.join_ref == socket.join_ref
+  end
 end
