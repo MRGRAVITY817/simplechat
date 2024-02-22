a3892110ae143561b8767ed55f3ec7e2c78e352f
Hoon Wee <mrgravity817@gmail.com>
2024-02-22 09:56:50 +0900

    feat: configure user_socket and room channel

--- lib/simplechat_web/endpoint.ex
+++ lib/simplechat_web/endpoint.ex
@@ -11,7 +11,14 @@ defmodule SimplechatWeb.Endpoint do
     same_site: "Lax"
   ]
 
-  socket "/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]]
+  socket "/live", Phoenix.LiveView.Socket,
+    websocket: [
+      connect_info: [session: @session_options]
+    ]
+
+  socket "/socket", ChatWeb.UserSocket,
+    websocket: true,
+    longpoll: false
 
   # Serve at "/" the static files from "priv/static" directory.
   #
