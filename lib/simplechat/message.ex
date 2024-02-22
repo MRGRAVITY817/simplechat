ccb1405a4159c9a7d3adf92fd85c139c3b0fb535
Hoon Wee <mrgravity817@gmail.com>
2024-02-22 14:23:44 +0900

    feat: fetch messages

--- lib/simplechat/message.ex
+++ lib/simplechat/message.ex
@@ -1,6 +1,7 @@
 defmodule Simplechat.Message do
   use Ecto.Schema
   import Ecto.Changeset
+  import Ecto.Query
 
   schema "messages" do
     field :message, :string
@@ -14,5 +15,17 @@ defmodule Simplechat.Message do
     message
     |> cast(attrs, [:name, :message])
     |> validate_required([:name, :message])
+  end
+
+  @doc """
+  Fetches all messages.
+  """
+  def get_messages(limit \\ 20) do
+    from(
+      m in Simplechat.Message,
+      order_by: [desc: m.inserted_at],
+      limit: ^limit
+    )
+    |> Simplechat.Repo.all()
   end
 end
